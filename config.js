f7a14240d1e0e292627575c915feaf4eab835cc9
qfsnesaragesara <support@qfsnesaragesara.org>
2026-01-17 12:17:00 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.__QFS_CONFIG = {
+  SUPABASE_URL: "https://qagktukzxtwbjrdgiben.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZ2t0dWt6eHR3YmpyZGdpYmVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4NDA0NTQsImV4cCI6MjA4MzQxNjQ1NH0.cbhSWHGlmhmIt-NmUVBUtAhPpNPDKk4Bz-Gy1TbPzHk"
+};
